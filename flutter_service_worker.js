5c5a4bb42bd00fdcfe8f32325fee8f5a6de5746d
Preethi Kulal <41234379+PreethiKulal@users.noreply.github.com>
2023-10-08 15:08:52 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,12 +29,12 @@ const RESOURCES = {"assets/AssetManifest.bin": "b042e415eb5851504ce4a247c4b7e342
 "icons/Icon-512.png": "740c027c83034c1b89529610f5d8f0d4",
 "icons/Icon-maskable-192.png": "08cd71fb61ad351aad2938950c199769",
 "icons/Icon-maskable-512.png": "14346c822e70aa67bd712b6e09b697b5",
-"index.html": "d4c3c0dc5db2de1f355f1895fc5f651a",
-"/": "d4c3c0dc5db2de1f355f1895fc5f651a",
-"main.dart.js": "d4e4b9b46aad3af07887aa821b8d5c50",
+"index.html": "9743b387a06fa817d91986f217eb656d",
+"/": "9743b387a06fa817d91986f217eb656d",
+"main.dart.js": "c7e9a21f3fdd85eb2e1ede30b45ee8ee",
 "manifest.json": "0d6f89bc1418025e2d5b20590e887877",
 "redirect.html": "19e98d49215b71d0b87a5bcb457a5550",
-"version.json": "0257a3e87fa89184fb893f4c1cba4696"};
+"version.json": "d809901a436fb298ac0f5e48033d164c"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
